ae403a039039b7c9fc7fe16df054fa158d06a31f
Waldomiro Barbosa Romão <35855708+dev-waldomiro@users.noreply.github.com>
2020-08-04 01:34:12 -0300

    [ADD] Panel and Panel Change
    Panel added. Panel sliding after user interaction added. Unkown error about IndexOutOfRangeException.

---
+++ casaVerde/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public RectTransform[] panelGroup;
+    private Vector2 translation = new Vector2();
+
+    private float changeTime;
+    private float delayToChange;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void movePanels () 
+    {
+        for(int i = 0; i <= panelGroup.Length; i++)
+        {
+            Debug.Log("hey, im here");
+            translation = panelGroup[i].anchoredPosition;
+            translation.x += -1200;
+            panelGroup[i].DOAnchorPos(translation,1f).SetDelay(0.25f);
+        }
+    }
+}
